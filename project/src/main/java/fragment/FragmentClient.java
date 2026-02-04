479c4300a29ad2bab212dfb8a23cfc95144c9ffb
Jayesh_Pandit <jayesh.pandit@iiitb.ac.in>
2026-02-04 17:32:37 +0530

    set up connections done

---
+++ project/src/main/java/fragment/FragmentClient.java
@@ -0,0 +1,125 @@
+package fragment;
+
+import java.sql.*;
+import java.util.*;
+
+public class FragmentClient {
+
+    private Map<Integer, Connection> connectionPool;
+    private Router router;
+    private int numFragments;
+
+    public FragmentClient(int numFragments) {
+        this.numFragments = numFragments;
+        this.router = new Router(numFragments);
+        this.connectionPool = new HashMap<>();
+    }
+
+    /**
+     * TODO: Initialize JDBC connections to all N Fragments.
+     */
+    public void setupConnections() {
+        try {
+            String user = "imt2023111";
+            String password = "secret";
+
+            for (int i = 0; i < numFragments; i++) {
+                String dbName = "frag" + i;
+                String url = "jdbc:postgresql://localhost:5432/" + dbName;
+
+                Connection conn = DriverManager.getConnection(url, user, password);
+                connectionPool.put(i, conn);
+
+                System.out.println("Connected to fragment " + i);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * TODO: Route the student to the correct shard and execute the INSERT.
+     */
+    public void insertStudent(String studentId, String name, int age, String email) {
+        try {
+            // Your code here:
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * TODO: Route the grade to the correct shard and execute the INSERT.
+     */
+    public void insertGrade(String studentId, String courseId, int score) {
+        try {
+            // Your code here
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateGrade(String studentId, String courseId, int newScore) {
+        try {
+            // Your code here:
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteStudentFromCourse(String studentId, String courseId) {
+        try {
+            // Your code here:
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * TODO: Fetch the student's name and email.
+     */
+    public String getStudentProfile(String studentId) {
+        try {
+            // Your code here
+            return null;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "ERROR";
+        }
+    }
+
+    /**
+     * TODO: Calculate the average score per department.
+     */
+    public String getAvgScoreByDept() {
+        try {
+            // Your code here
+            return null;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "ERROR";
+        }
+    }
+
+    /**
+     * TODO: Find all the students that have taken most number of courses
+     */
+    public String getAllStudentsWithMostCourses() {
+        try {
+            // Your code here
+            return null;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "ERROR";
+        }
+    }
+
+    public void closeConnections() {
+
+    }
+}
